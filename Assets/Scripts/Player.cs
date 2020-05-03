05996fb08777bab2a6ded7d26b052f59f1f71579
critter17 <chrisritter17@gmail.com>
2020-05-02 23:53:38 -0500

    Created Player Prefab, created player script with movement, plus some walking animations using a placeholder asset.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Variables
+    [SerializeField] float speed = 5f;
+    private bool moving;
+    [SerializeField] private Vector2 lastMove;
+
+    // Cached References
+    private Rigidbody2D playerRigidBody;
+    private Animator playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        moving = false;
+        lastMove = new Vector2(0f, -1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// The player moves based on input
+    /// </summary>
+    private void Move()
+    {
+        float horizontal = Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
+        float vertical = Mathf.RoundToInt(Input.GetAxisRaw("Vertical"));
+
+        if (horizontal != 0)
+        {
+            playerRigidBody.MovePosition(new Vector2(transform.position.x + (horizontal * speed * Time.deltaTime), transform.position.y));
+            moving = true;
+            lastMove = new Vector2(horizontal, 0f);
+        }
+        else if (vertical != 0)
+        {
+            playerRigidBody.MovePosition(new Vector2(transform.position.x, transform.position.y + (vertical * speed * Time.deltaTime)));
+            moving = true;
+            lastMove = new Vector2(0f, vertical);
+        }
+        else
+            moving = false;
+
+        playerAnimator.SetFloat("Horizontal", horizontal);
+        playerAnimator.SetFloat("Vertical", vertical);
+        playerAnimator.SetFloat("LastMoveHorizontal", lastMove.x);
+        playerAnimator.SetFloat("LastMoveVertical", lastMove.y);
+        playerAnimator.SetBool("Walking", moving);
+    }
+}
