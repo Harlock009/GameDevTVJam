148ab41e44928aefb8565b81f3efdf24990461c4
5war00p <57614947+5war00p@users.noreply.github.com>
2020-05-07 17:27:53 +0530

    Font Styles for text & Timer
    Timer decrement set by creating GameSession gameObject and GameSession script.
    
    GameText folder created and Title and Text format inserted.

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,39 @@
+﻿using TMPro;
+using UnityEngine;
+public class GameSession : MonoBehaviour
+{
+    [SerializeField] float timeInMinutes = 1f;
+    [SerializeField] TextMeshProUGUI timerText = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeInMinutes *= 60f;
+        timerText.text = timeInMinutes.ToString();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer();
+    }
+
+    private void Timer()
+    {
+        timeInMinutes -= Time.deltaTime;
+
+        float minutes = Mathf.Floor(timeInMinutes / 60);
+        float seconds = Mathf.FloorToInt(timeInMinutes % 60);
+        float milliseconds = Mathf.FloorToInt((timeInMinutes * 100) % 100);
+
+        if(minutes == 0 && seconds == 0 && milliseconds == 0)
+        {
+            Destroy(gameObject); //Example task..
+        }
+        if(seconds == 60)
+        {
+            seconds = 0;
+        }
+
+        timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("00");
+    }
+}
