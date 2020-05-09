55a717b9f969e9d12d2a50a3ca98d5de380e8478
critter17 <chrisritter17@gmail.com>
2020-05-09 02:42:17 -0500

    The Three Wise Monkey Puzzle. Created Pedestal prefab. Created Wise Monkey Puzzle prefab. Created Puzzle folder for puzzle scripts. Changed Player prefab to have dynamic Rigidbody Changed player speed from 3 to 4.

---
+++ Assets/Scripts/Puzzles/Wise Monkey Puzzle/WiseMonkeyPuzzle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiseMonkeyPuzzle : MonoBehaviour
+{
+    public WiseMonkeysClue wiseMonkeyClue;
+    public WiseMonkeys wiseMonkeys;
+
+    // List of sprites used for the puzzle
+    public List<Sprite> monkeySprites;
+    public bool puzzleCompleted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        puzzleCompleted = false;
+
+        if (wiseMonkeyClue != null)
+            wiseMonkeyClue.SetCorrectOrder();
+
+        if (wiseMonkeys != null)
+            wiseMonkeys.RandomizeMonkeys();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Puzzles/Wise Monkey Puzzle/WiseMonkeys.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WiseMonkeys : MonoBehaviour
+{
+    [SerializeField] private WiseMonkeyPuzzle wiseMonkeyPuzzle = null;
+    [SerializeField] private Monkey[] monkeys = null;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void RandomizeMonkeys()
+    {
+        if (wiseMonkeyPuzzle == null) return;
+
+        List<Sprite> tempSprites = new List<Sprite>(wiseMonkeyPuzzle.monkeySprites);
+
+        for (int idx = 0; idx < wiseMonkeyPuzzle.monkeySprites.Count; idx++)
+        {
+            int monkeyIdx = Random.Range(0, tempSprites.Count);
+            monkeys[idx].monkeySprite.sprite = tempSprites[monkeyIdx];
+            tempSprites.RemoveAt(monkeyIdx);
+        }
+
+        for (int i = 0; i < wiseMonkeyPuzzle.monkeySprites.Count; i++)
+        {
+            for (int j = 0; j < monkeys.Length; j++)
+            {
+                if (wiseMonkeyPuzzle.monkeySprites[i] == monkeys[j].monkeySprite.sprite)
+                    monkeys[j].CurrentMonkey = i;
+            }
+        }
+
+        // To guarantee that the monkeys do not start in the correct positon,
+        // swap the first monkey with the second if it matches the correct monkey
+        if (monkeys[0].monkeySprite.sprite == wiseMonkeyPuzzle.wiseMonkeyClue.monkeyOrder[0])
+        {
+            Sprite temp = monkeys[0].monkeySprite.sprite;
+            int tempCurrentMonkey = monkeys[0].CurrentMonkey;
+
+            monkeys[0].monkeySprite.sprite = monkeys[1].monkeySprite.sprite;
+            monkeys[0].CurrentMonkey = monkeys[1].CurrentMonkey;
+            monkeys[1].monkeySprite.sprite = temp;
+            monkeys[1].CurrentMonkey = tempCurrentMonkey;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void CheckOrder()
+    {
+        bool match = true;
+
+        for (int idx = 0; idx < monkeys.Length; idx++)
+        {
+            if (monkeys[idx].monkeySprite.sprite != wiseMonkeyPuzzle.wiseMonkeyClue.monkeyOrder[idx])
+            {
+                match = false;
+                break;
+            }
+        }
+
+        if (match)
+        {
+            wiseMonkeyPuzzle.puzzleCompleted = true;
+        }
+    }
+}
