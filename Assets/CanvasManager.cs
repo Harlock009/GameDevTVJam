e202b6ea5ef084972605e4e69baec153927150bd
critter17 <chrisritter17@gmail.com>
2020-05-17 02:28:35 -0500

    Adding Credits canvas to StartGame scene Created CanvasManager for changing between canvases

---
+++ Assets/CanvasManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    [SerializeField] GameObject titleCanvas;
+    [SerializeField] GameObject creditsCanvas;
+
+    public void OnCreditsButton()
+    {
+        titleCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
+
+    public void OnBackButton()
+    {
+        titleCanvas.SetActive(true);
+        creditsCanvas.SetActive(false);
+    }
+}
